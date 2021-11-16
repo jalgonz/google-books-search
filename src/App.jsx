87986257af7282985954964f17809de2be21da6f
Joshua Gonzalez <jalgonz96@gmail.com>
2021-11-15 19:21:09 -0500

    switched over to jsx and scss

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/navbar/Navbar";
+import './app.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar/>
+      <div className="sections">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
